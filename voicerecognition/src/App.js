d6c70ca26daf24a4c1dfb87f6293ed434cc8e999
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-15 22:03:46 +0000

    not fetching

--- voicerecognition/src/App.js
+++ voicerecognition/src/App.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {};
+
+  }
+  onListeClick(){
+  
+    
+   // fetch(' https://gateway-lon.watsonplatform.net/speech-to-text/api')
+   fetch('http://localhost:3002/api/speech-to-text/token')
+  .then(function(response) {
+      return response.text();
+  }).then((token)=> {
+    var stream = recognizeMic({
+        token: token, // use `access_token` as the parameter name if using an RC service
+        objectMode: true, // send objects instead of text
+        extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
+        format: false // optional - performs basic formatting on the results such as capitals an periods
+    });
+    stream.on('data', (data) => {
+      console.log(data);
+      this.setState({
+    text:data.alternatives[0].transcript
+        })   
+    });
+    stream.on('error', function(err) {
+        console.log(err);
+    });
+    document.querySelector('#stop').onclick = stream.stop.bind(stream);
+  }).catch(function(error) {
+      console.log(error);
+  });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+    <button onClick={this.onListeClick.bind(this)}>Listen to michrofone </button>
+    <div> {this.state.text}</div>
         </header>
       </div>
     );
